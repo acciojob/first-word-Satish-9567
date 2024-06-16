deea12158516eb89d9cdb5779bb17a4fcc516718
Satish-9567 <150673586+Satish-9567@users.noreply.github.com>
2024-06-16 22:29:08 +0530

    done with work

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function firstWord(s) {
   // your code here
+  s=s.trim();
+  let fsi=s.indexOf(' ');
+  if(fsi===-1)
+    return s;
+  return s.substring(0.fsi);
 }
 
 // Do not change the code below
